003ecd52e6560eb53b9df021817ea082850405e6
Rodgers Kipchumba <rodgers@Rodgerss-MacBook-Pro.local>
2023-09-15 14:26:39 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // You can style your components using CSS in this file.
 
 function App() {
+  const [examStarted, setExamStarted] = useState(false);
+
+  const startExam = () => {
+    setExamStarted(true);
+    // You can add logic to initiate the oral exam here.
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Oral Exam App</h1>
+        {!examStarted ? (
+          <button onClick={startExam}>Start Oral Exam</button>
+        ) : (
+          <p>Oral exam is in progress...</p>
+        )}
       </header>
     </div>
   );
---
+++ src/ChatInterface.js
@@ -0,0 +1,14 @@
+// ChatInterface.js
+
+import React from 'react';
+
+function ChatInterface() {
+  // Your chat interface component goes here
+  return (
+    <div>
+      {/* Your chat interface content */}
+    </div>
+  );
+}
+
+export default ChatInterface;
